891b0af8b7c7e09bd9df087633facec45f03d7a1
PedrosPinho <pedro.ads652@gmail.com>
2018-10-15 20:27:38 -0300

    i guess i'll just die

--- iinv-barJFX/src/application/Main.java
+++ iinv-barJFX/src/application/Main.java
@@ -1,16 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			AnchorPane root = (AnchorPane) FXMLLoader.load(Main.class.getResource("/application/login.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
