eb8a8b0ba0e13cdfd5c8c53cf0cb56177ebd9428
Felipe M. <felipe.morassutti@hotmail.com>
2018-10-22 16:09:29 -0300

    Clases criadas(mais ou menos ---> 1o cp)

---
+++ INV/src/inv/Desconto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inv;
+
+
+public class Desconto {
+ 
+    private int porcentagem;
+    private int frequencia;
+    
+     public int getPorcentagem() {
+    return this.porcentagem;
+  }
+    public void setPorcentagem (int porcentagem) {
+    this.porcentagem = porcentagem;
+    }
+    public int getFrequencia() {
+    return this.frequencia;
+    }
+    public void setFrequencia (int frequencia) {
+    this.frequencia = frequencia;
+    
+}
+}
+
