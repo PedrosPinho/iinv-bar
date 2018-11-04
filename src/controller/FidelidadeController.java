7fcb1c0dcf16c2cd6dae14df8c6f8ede51efe281
mateusjuristec <mateus.m@juristecplus.com.br>
2018-11-04 14:27:19 -0200

    view fidelidade done

--- src/controller/FidelidadeController.java
+++ src/controller/FidelidadeController.java
@@ -1,5 +1,29 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class FidelidadeController {
+
+    @FXML
+    private Label lblDesconto;
+
+    @FXML
+    private Button btnDesconto;
+
+    @FXML
+    private ImageView btnVoltar;
+
+    @FXML
+    private TextField tfDesconto;
+
+    @FXML
+    private Label lblFrequencia;
+
+    @FXML
+    private TextField tfFrequencia;
 
 }
