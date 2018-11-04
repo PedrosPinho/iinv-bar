f584ff67a2e5245994a8f95e68401f40fe4f33de
PedrosPinho <pedro.ads652@gmail.com>
2018-11-04 14:11:52 -0200

    cardapio table view

--- src/controller/CardapioController.java
+++ src/controller/CardapioController.java
@@ -23,6 +23,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -30,7 +31,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CardapioController {
 
-	public CardapioController () throws IOException, ParseException {
+	@SuppressWarnings("unchecked")
+	public void initialize () throws IOException, ParseException {
 		ArrayList<Object> al = new ArrayList();
 		URL url = new URL("http://localhost:5000/iinv-bar/us-central1/cardapio/");
     	HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -54,8 +56,17 @@ public class CardapioController {
     		Cardapio c = gson.fromJson(a.toString(), Cardapio.class);
     		data.add(c);
     	});
-    	this.tbCardapio.getColumns().
-    	this.tbCardapio.setItems(data);
+    	System.out.print(data);
+    	this.tcNome.setCellValueFactory(new PropertyValueFactory<Cardapio, String>("Nome"));
+    	this.tcDescricao.setCellValueFactory(new PropertyValueFactory<Cardapio, String>("Descricao"));
+    	this.tcPreco.setCellValueFactory(new PropertyValueFactory<Cardapio, String>("Preco"));
+    	this.tbCardapio.getItems().setAll(data);
+	    	
+
+    	
+//    	this.tbCardapio = (TableView<Cardapio>) data;
+
+
     	in.close();
 	}
 	
@@ -76,6 +87,15 @@ public class CardapioController {
 
     @FXML
     private TableView<Cardapio> tbCardapio;
+    
+    @FXML
+    private TableColumn<Cardapio,String> tcNome;
+    
+    @FXML
+    private TableColumn<Cardapio,String> tcPreco;
+    
+    @FXML
+    private TableColumn<Cardapio,String> tcDescricao;
     
     @FXML
     private Label lblPreco;
